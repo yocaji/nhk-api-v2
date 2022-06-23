76894766676e56d9faf1174317de4f0741074df3
yocajii <yocajii@stone.sakura.ne.jp>
2022-06-23 14:55:56 +0900

    利用例を作成した

---
+++ example/nhk-api-v2-example.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const NhkApiV2 = require('nhk-api-v2')
+
+const defaultClient = NhkApiV2.ApiClient.instance
+const api_key = defaultClient.authentications.api_key
+api_key.apiKey = process.env.API_KEY
+
+const api = new NhkApiV2.ProgramsApi()
+const area = '020' // {Number} ID of broadcast area
+const service = 'tv' // {String} ID of channel or channel group
+const date = '2022-06-24' // {String} Date in YYYY-MM-DD format
+const genre = '0000' // {String} Genre code defined by ARIB STD-B10
+const id = '2022062532788' // {String} ID of channel
+
+api.getPrograms(area, service, date).then(function (data) {
+  console.log(`${date} にEテレ1で放送される21番目の番組のジャンルコード: ` + data.list.e1[20].genres)
+  // 2022-06-24 にEテレ1で放送される21番目の番組のジャンルコード: 1008,0300
+}, function (error) {
+  console.error(error)
+})
+api.getProgramsByGenre(area, service, date, genre).then(function (data) {
+  console.log(`${date} にBS1で放送される「定時・総合」ジャンルのうち7番目の番組のサブタイトル: ` + data.list.s1[6].subtitle)
+  // 2022-06-24 にBS1で放送される「定時・総合」ジャンルのうち7番目の番組のサブタイトル:
+  // ▽国内外のニュースや気象情報をお伝えします。　※スポーツ中継がある場合は、放送時間や内容が変更になります。
+}, function (error) {
+  console.error(error)
+})
+api.getProgramInfo(area, service, id).then(function (data) {
+  console.log(`番組ID ${id} の開始時刻: ` + data.list.g1[0].start_time)
+  // 番組ID 2022062532788 の開始時刻: 2022-06-25T05:00:00+09:00
+}, function (error) {
+  console.error(error)
+})
+api.getProgramsNowOnAir(area, service).then(function (data) {
+  console.log('総合1で放送中の番組タイトル: ' + data.nowonair_list.g1.present.title)
+  // 総合1で放送中の番組タイトル: ニュース６４５
+}, function (error) {
+  console.error(error)
+})
